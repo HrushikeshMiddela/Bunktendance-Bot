b698b4a6ad9e1dc196f66197b7011582e67e1969
HrushikeshMiddela <middelahrushikeshsai@gmail.com>
2025-03-08 10:51:49 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+// src/utils/logger.js
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'bot.log' })
+  ],
+});
+
+module.exports = logger;
